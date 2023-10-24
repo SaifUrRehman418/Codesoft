03462f28485ea07672134ae9d48f44675ca82bc1
SaifUrRehman418 <145798562+SaifUrRehman418@users.noreply.github.com>
2023-10-24 09:36:13 -0700

    Add files via upload

---
+++ Task 2 Webpage/index.js
@@ -0,0 +1,7 @@
+const prompt =require("prompt-sync")();
+
+a={
+    city :"Faisalabad",
+    Country: "Pakistan",
+}
+console.log(a.city);
---
+++ Task 2 Webpage/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+var a=[1,3,4];
+var b= a.concat[5,6,7];
+console.log(b);
